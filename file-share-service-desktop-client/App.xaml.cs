a497c3711ab6615e268af2d52ca64257ec3b3d4f
usha13529 <46884107+usha13529@users.noreply.github.com>
2021-11-24 19:29:29 +0530

    AB#11872-Changes to replace App.config file with appsettings.json file and to include in fss tooling installer. (#17)
    * AB#11872-Changes to add App.config file to fss tooling installer.
    
    * AB#-11872-Changes made as per review comment to include FileShareService.DesktopClient.dll.config in installer instead of App.config file.
    
    * Use appsettings.json for config instead of app.config. The advantage of appsettings.json is the base path (i.e. the folder containing the exe and json files) can be configured during startup, which is needed when the app is published with PublishSingleFile. I could not get the older App.config/ConfigurationManager approach to work with PublishSingleFile
    
    * Add to appsettings.json the config values needed in Chaitali's branch
    
    Co-authored-by: usha13529 <usha13529@Mastek.com>
    Co-authored-by: Chris Dando <christopher.dando@ukho.gov.uk>

--- file-share-service-desktop-client/App.xaml.cs
+++ file-share-service-desktop-client/App.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.IO;
 using System.IO.Abstractions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Markup;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Prism.Ioc;
@@ -36,23 +36,25 @@ namespace UKHO.FileShareService.DesktopClient
 
         protected override IContainerExtension CreateContainerExtension()
         {
-            //            var containerExtension = base.CreateContainerExtension() as UnityContainerExtension;
-            //#if DEBUG
-            //            containerExtension.Instance.AddExtension(new Diagnostic());
-            //#endif
-            //            return containerExtension;
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
 
             var serviceCollection = new ServiceCollection();
-
             serviceCollection.AddLogging(loggingBuilder => {
                 var logger = new LoggerConfiguration()
-                        .ReadFrom.AppSettings()
+                        .ReadFrom.Configuration(configuration)
                         .CreateLogger();
                 loggingBuilder.AddSerilog(logger, dispose: true);
             });
 
+            var retryCount = configuration.GetValue<int>("RetryCount");
+            var sleepDurationMultiplier = configuration.GetValue<int>("SleepDurationMultiplier");
+
             var container = new UnityContainer();
             container.BuildServiceProvider(serviceCollection);
+            container.RegisterInstance<IConfiguration>(configuration);
 
             return new UnityContainerExtension(container);
         }
