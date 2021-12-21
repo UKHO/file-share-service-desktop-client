c1087b984e4733e653938af7848694246047d972
Pradnya Puranik <Pradnya13705@Mastek.Com>
2021-12-21 10:09:45 +0530

    AB#12431-Focus Should move to Alert Massage

--- file-share-service-desktop-client/Modules/Admin/AdminView.xaml.cs
+++ file-share-service-desktop-client/Modules/Admin/AdminView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace UKHO.FileShareService.DesktopClient.Modules.Admin
 {
@@ -13,6 +15,40 @@ namespace UKHO.FileShareService.DesktopClient.Modules.Admin
         {
             DataContext = viewModel;
             InitializeComponent();
+        }
+
+        private void pnlExecutionComplete_IsVisibleChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            Border border = sender as Border;
+            TextBox txtBatchCommitResponse = border.FindName("txtBatchCommitResponse") as TextBox;
+            if (border.IsVisible && txtBatchCommitResponse != null)
+            {
+                txtBatchCommitResponse.Dispatcher.BeginInvoke(
+                new Action(
+                delegate
+                {
+                    Keyboard.ClearFocus();
+                    Keyboard.Focus(txtBatchCommitResponse);
+                    bool flag = txtBatchCommitResponse.Focus();
+                }));
+            }
+        }
+
+        private void txtBatchCommitInProgressResponse_IsVisibleChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            TextBox txtBatchCommitInProgressResponse = sender as TextBox;
+            
+            if (txtBatchCommitInProgressResponse.IsVisible)
+            {
+                txtBatchCommitInProgressResponse.Dispatcher.BeginInvoke(
+                new Action(
+                delegate
+                {
+                    Keyboard.ClearFocus();
+                    Keyboard.Focus(txtBatchCommitInProgressResponse);
+                    bool flag = txtBatchCommitInProgressResponse.Focus();
+                }));
+            }
         }
     }
 }
