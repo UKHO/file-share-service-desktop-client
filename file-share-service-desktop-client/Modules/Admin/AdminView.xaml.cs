dbf2d516c993e6b956a3fc148ece0e7c42c4d2b3
JiviteshT <112479261+JiviteshT@users.noreply.github.com>
2024-05-31 13:58:32 +0100

    Feature/abzu 158716 ignore duplicate check (#70)
    * Abzu-reordered assembly version step + NVD
    
    * Abzu-148955 - removed coverity
    
    * Abzu-148955-Implemented functionality to ignore duplicate batch search
    
    * Abzu-158716-Implemented functionality to ignore duplicate batch search
    
    * Abzu-158716-NVD

--- file-share-service-desktop-client/Modules/Admin/AdminView.xaml.cs
+++ file-share-service-desktop-client/Modules/Admin/AdminView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -11,12 +12,26 @@ namespace UKHO.FileShareService.DesktopClient.Modules.Admin
     [ExcludeFromCodeCoverage]//Basic View
     public partial class AdminView : UserControl
     {
-        public AdminView(AdminViewModel viewModel)
+        private readonly IMessageBoxService messageBoxService;
+        
+        public AdminView(AdminViewModel viewModel, IMessageBoxService messageBoxService)
         {
             DataContext = viewModel;
             InitializeComponent();
+            this.messageBoxService = messageBoxService;
         }
 
+        private void CheckBoxChanged(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+
+            if (messageBoxService.ShowMessageBox($"Confirmation",
+                    $"Upload without checking for duplicates",
+                    MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.Cancel)
+            {
+                checkBox.IsChecked = false;
+            }
+        }
 
         private void handleAlertFocus(Border? border, string alertMessageTextBoxName)
         {
